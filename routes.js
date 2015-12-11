fc7dc85fff70fc2768f19a55cce78c9cfeef7b4f
NullCodex <jamesonyu95@gmail.com>
2015-12-10 21:34:07 -0500

    Modified code to act as an API
    - Removed img
    - Required io dependencies
    - removed htmls

--- routes.js
+++ routes.js
@@ -2,33 +2,10 @@
 // It sets up event listeners
 // For the different URL endpoints of the application
 
-module.exports = function(app, io, db) {
+module.exports = function(app, db) {
 
     app.get('/', function (req, res) {
-        res.render('login');
-
-    });
-
-    app.get('/game', function (req, res) {
-        res.render('main');
-    });
-
-    app.post('/adduser', function (req, res) {
-
-        var playerName = req.body.playerName;
-        var searchQuery = {};
-
-        searchQuery["playerName"] = playerName;
-
-        collection = db.collection('war');
-
-        collection.find(searchQuery).toArray(function(err, docs) {
-            if(docs.length != 0) {
-                alert("Name is already taken!");
-            } else {
-                res.redirect('game');
-            }
-        });
+        res.sendStatus(204);
 
     });
 
